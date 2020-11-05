758590f1c794df2a4c7f9adab89ace67af2469de
한진호 <tlfanfvk@naver.com>
2020-11-05 16:21:33 +0900

    데이터 제어 Filtering

--- src/main/java/com/jinho/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/jinho/restfulwebservice/user/UserDaoService.java
@@ -14,9 +14,9 @@ public class UserDaoService {
     private static int userCount = 3;
 
     static {
-        users.add(new User(1, "Jinho", new Date()));
-        users.add(new User(2, "Jinho", new Date()));
-        users.add(new User(3, "Jinho", new Date()));
+        users.add(new User(1, "Jinho", new Date(), "pass1" ,"701111-111111"));
+        users.add(new User(2, "Jinho", new Date(), "pass1", "601111-111111"));
+        users.add(new User(3, "Jinho", new Date(), "pass1", "501111-111111"));
     }
 
     public List<User> findAll() {
